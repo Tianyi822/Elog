29a1d21b8a1bf270211cdcc418eda3ca63f4f931
chentianyi <chentyit@163.com>
2023-10-18 19:28:10 +0800

    添加对文件的一些操作函数

---
+++ file-sys/libs.go
@@ -0,0 +1,34 @@
+package file_sys
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+)
+
+// IsExists 判断路径是否存在
+func IsExists(path string) bool {
+	_, err := os.Stat(path)
+	return !errors.Is(err, fs.ErrNotExist)
+}
+
+// MustOpenFile 直接打开文件，使用该方法的前提是确定文件一定存在
+func MustOpenFile(path string) (*os.File, error) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_RDWR, 0666)
+	return file, err
+}
+
+// CreateFile 创建文件，先检查文件是否存在，存在就报错，不存在就创建
+func CreateFile(path string) (*os.File, error) {
+	exist := IsExists(path)
+	if exist {
+		return nil, errors.New("file already exists")
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
