e937bae7d627a2786db6761de5cab8d8df99e802
chentianyi <chentyit@163.com>
2023-10-19 22:39:55 +0800

    修改测试用例

--- file-sys/file_test.go
+++ file-sys/file_test.go
@@ -3,7 +3,7 @@ package file_sys
 import "testing"
 
 func TestCreateFileOp(t *testing.T) {
-	path := "E:\\MineProgram\\go-workplace\\easy-go-log\\log\\test.log"
+	path := "/Users/chentianyi/Program/Goland-workplace/easy-go-log/log/test.log"
 
 	fo := CreateFileOp(path, 1, false)
 	err := fo.ready()
