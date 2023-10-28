4ad2256f1ffa69a3500afe05956e41309a94e716
chentianyi <chentyit@163.com>
2023-10-28 14:57:26 +0800

    根据新的 CreateFile 逻辑重写测试用例

--- file-sys/file_test.go
+++ file-sys/file_test.go
@@ -3,9 +3,9 @@ package file_sys
 import "testing"
 
 func TestCreateFileOp(t *testing.T) {
-	path := "/Users/chentianyi/Program/Goland-workplace/easy-go-log/log/test.log"
+	path := "/Users/chentianyi/Program/Goland-workplace/easy-go-log/log"
 
-	fo := CreateFileOp(path, 1, false)
+	fo := CreateFileOp(path, "test.elog", 1, false)
 	err := fo.ready()
 	if err != nil {
 		t.Fatalf("日志文件准备发生错误: %v", err)
