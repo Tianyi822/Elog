bb59970c6cfb76fbe07f066edb19c3d82992e16a
chentianyi <chentyit@163.com>
2023-10-28 15:52:15 +0800

    添加 TestFileOp_Write 单元测试

--- file-sys/file_test.go
+++ file-sys/file_test.go
@@ -11,3 +11,18 @@ func TestCreateFileOp(t *testing.T) {
 		t.Fatalf("日志文件准备发生错误: %v", err)
 	}
 }
+
+func TestFileOp_Write(t *testing.T) {
+	path := "/Users/chentianyi/Program/Goland-workplace/easy-go-log/log"
+
+	fo := CreateFileOp(path, "test.elog", 1, false)
+	err := fo.ready()
+	if err != nil {
+		t.Fatalf("日志文件准备发生错误: %v", err)
+	}
+
+	err = fo.Write([]byte("test"))
+	if err != nil {
+		t.Fatalf("写入日志发生错误: %v", err)
+	}
+}
