1ae3f33d92dfe85b84c93a22958c66e26ba31dca
chentianyi <chentyit@163.com>
2023-10-28 15:27:47 +0800

    添加关闭文件和写文件方法

--- file-sys/file.go
+++ file-sys/file.go
@@ -45,3 +45,77 @@ func (fo *FileOp) ready() (err error) {
 	fo.curDate = time.Now()
 	return nil
 }
+
+// Close 关闭文件
+func (fo *FileOp) Close() error {
+	fo.isOpen = false
+	err := fo.file.Close()
+	fo.file = nil
+	return err
+}
+
+// needSplit 判断是否需要进行分片
+func (fo *FileOp) needSplit() bool {
+	// 判断是否需要进行分片
+	if fo.maxSize <= 0 {
+		return false
+	}
+
+	// 判断是否需要进行分片
+	if fo.curDate.Day() != time.Now().Day() {
+		return true
+	}
+
+	// 判断文件大小是否超过最大值
+	fileInfo, err := fo.file.Stat()
+	if err != nil {
+		return false
+	}
+
+	return fileInfo.Size() > int64(fo.maxSize*1024*1024)
+}
+
+// Write 写入数据
+// 该函数不做并发处理，传入的数据都是通过 channel 传递过来的，所以不需要考虑并发问题
+// 并不会出现多个协程往同一个文件里面写数据，文件操作模块主要集中于对日志文件的分片管理，对历史日志打包
+func (fo *FileOp) Write(context []byte) error {
+	if !fo.isOpen {
+		return fo.ready()
+	}
+
+	// 判断是否需要进行分片
+	if fo.needSplit() {
+		// 关闭文件
+		err := fo.file.Close()
+		if err != nil {
+			return err
+		}
+
+		// 重命名文件
+		src := fo.path + "/" + fo.fileName
+		dst := fo.path + "/" + fo.fileName + "_" + fo.curDate.Format("2006-01-02_15:04:05")
+		err = os.Rename(src, dst)
+		if err != nil {
+			return err
+		}
+
+		// 压缩文件
+		if fo.needCompress {
+			err = Compress(dst, dst+".zip")
+			if err != nil {
+				return err
+			}
+		}
+
+		// 重新打开文件
+		err = fo.ready()
+		if err != nil {
+			return err
+		}
+	}
+
+	// 写入数据
+	buf := append(context, '\n')
+	_, err := fo.file.Write(buf)
+	return err
+}
