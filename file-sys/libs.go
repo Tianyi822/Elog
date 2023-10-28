439a33d881bf5dbcd351df779e9cb892b771d385
chentianyi <chentyit@163.com>
2023-10-28 14:56:59 +0800

    完善 CreateFile 逻辑： 1. 路径不存在创建路径，不再报错 2. 文件不存在就创建文件，不再报错

--- file-sys/libs.go
+++ file-sys/libs.go
@@ -19,10 +19,25 @@ func MustOpenFile(path string) (*os.File, error) {
 }
 
 // CreateFile 创建文件，先检查文件是否存在，存在就报错，不存在就创建
-func CreateFile(path string) (*os.File, error) {
+func CreateFile(path, fileName string) (*os.File, error) {
 	exist := IsExists(path)
+	if !exist {
+		err := os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// 文件路径
+	path = path + "/" + fileName
+	exist = IsExists(path)
 	if exist {
-		return nil, errors.New("file already exists")
+		return nil, errors.New("文件已存在")
+	} else {
+		_, err := os.Create(path)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
