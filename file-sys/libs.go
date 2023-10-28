d74bb84b02ad697b09a505c9d788f8ee29e337a6
chentianyi <chentyit@163.com>
2023-10-28 15:06:18 +0800

    添加压缩和解压文件函数

--- file-sys/libs.go
+++ file-sys/libs.go
@@ -1,9 +1,14 @@
 package file_sys
 
 import (
+	"archive/zip"
 	"errors"
+	"io"
 	"io/fs"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 // IsExists 判断路径是否存在
@@ -46,4 +51,145 @@ func CreateFile(path, fileName string) (*os.File, error) {
 	}
 
 	return file, nil
+}
+
+// Remove 删除文件
+func Remove(path string) error {
+	return os.RemoveAll(path)
+}
+
+// Compress
+// @param filePath 需要压缩文件或者目录的路径
+// @param dest 压缩目标文件
+// @description 压缩文件
+func Compress(pkgPath string, paths ...string) error {
+	// 获取上级目录路径
+	preDir := filepath.Dir(pkgPath)
+	if err := os.MkdirAll(preDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	// 创建压缩文件
+	archive, err := os.Create(pkgPath)
+	if err != nil {
+		return err
+	}
+	defer func(archive *os.File) {
+		_ = archive.Close()
+	}(archive)
+
+	// 创建 zip writer
+	zipWriter := zip.NewWriter(archive)
+	defer func(zipWriter *zip.Writer) {
+		_ = zipWriter.Close()
+	}(zipWriter)
+
+	// 遍历需要打包的路径
+	for _, srcPath := range paths {
+		// 删除最后一个 '/'
+		srcPath = strings.TrimSuffix(srcPath, string(os.PathSeparator))
+
+		// 开始检查文件树
+		err = filepath.Walk(
+			srcPath,
+			func(path string, info fs.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				header, err := zip.FileInfoHeader(info)
+				if err != nil {
+					return err
+				}
+
+				// 设置压缩方式
+				header.Method = zip.Deflate
+
+				// 将文件的相对路径设置为头名称
+				header.Name, err = filepath.Rel(filepath.Dir(srcPath), path)
+				if err != nil {
+					return err
+				}
+				if info.IsDir() {
+					header.Name += string(os.PathSeparator)
+				}
+
+				// 创建文件头写入器并保存文件内容
+				headerWriter, err := zipWriter.CreateHeader(header)
+				if err != nil {
+					return err
+				}
+				if info.IsDir() {
+					return nil
+				}
+				f, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer func(f *os.File) {
+					_ = f.Close()
+				}(f)
+				_, err = io.Copy(headerWriter, f)
+				return err
+			})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Decompress
+// @param srcPath 压缩包路径
+// @param dstPath 解压路径
+func Decompress(srcPath, dstPath string) error {
+	reader, err := zip.OpenReader(srcPath)
+	if err != nil {
+		return err
+	}
+	defer func(reader *zip.ReadCloser) {
+		_ = reader.Close()
+	}(reader)
+	for _, file := range reader.File {
+		if err := decompress(file, dstPath); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func decompress(file *zip.File, dstPath string) error {
+	// create the directory of file
+	filePath := path.Join(dstPath, file.Name)
+	if file.FileInfo().IsDir() {
+		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
+			return err
+		}
+		return nil
+	}
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		return err
+	}
+
+	// open the file
+	r, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer func(r io.ReadCloser) {
+		_ = r.Close()
+	}(r)
+
+	// create the file
+	w, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer func(w *os.File) {
+		_ = w.Close()
+	}(w)
+
+	// save the decompressed file content
+	_, err = io.Copy(w, r)
+	return err
 }
