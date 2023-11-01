b752d11efe9102db2c3c3c257885fed273bbdd5a
chentianyi <chentyit@163.com>
2023-11-01 15:25:40 +0800

    修复压缩文件代码BUG

--- file-sys/file.go
+++ file-sys/file.go
@@ -96,8 +96,9 @@ func (fo *FileOp) Write(context []byte) error {
 		if fo.needCompress {
 			// 获取文件名和后缀
 			fInfo := strings.Split(fo.fileName, ".")
+			src := filepath.Join(fo.dirPath, fo.fileName)
 			dst := filepath.Join(fo.dirPath, fInfo[0]+"_"+time.Now().Format("2006-01-02_15:04:05")+"."+fInfo[1])
-			err = Compress(dst+".zip", dst)
+			err = Compress(dst+".zip", src)
 			if err != nil {
 				return err
 			}
