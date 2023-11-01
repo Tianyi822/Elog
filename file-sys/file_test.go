f7e0f6f97fed8b2a4a06c5a6ec61bc36591501dd
chentianyi <chentyit@163.com>
2023-11-01 15:14:41 +0800

    删除多余操作

--- file-sys/file_test.go
+++ file-sys/file_test.go
@@ -29,13 +29,8 @@ func TestFileOp_Write(t *testing.T) {
 	path := "/Users/chentianyi/Program/Goland-workplace/easy-go-log/log"
 
 	fo := CreateFileOp(path, "test.elog", 1, true)
-	err := fo.ready()
-	if err != nil {
-		t.Fatalf("日志文件准备发生错误: %v", err)
-	}
-
 	for {
-		err = fo.Write([]byte("test"))
+		err := fo.Write([]byte("test"))
 		if err != nil {
 			t.Fatalf("写入日志发生错误: %v", err)
 		}
