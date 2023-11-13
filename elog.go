066fd88dd67c4d21ba6e0a2aad052fb8803de907
chentianyi <chentyit@163.com>
2023-11-13 12:32:54 +0800

    添加日志格式化组件

--- elog.go
+++ elog.go
@@ -3,6 +3,8 @@ package elog
 import (
 	"fmt"
 	"gitee.com/xxc_opensource/elog/core"
+	"gitee.com/xxc_opensource/elog/format"
+	"time"
 )
 
 type LogLevel byte
@@ -17,15 +19,17 @@ const (
 )
 
 type Elog struct {
-	Level LogLevel
-	core  *core.Core
+	Level  LogLevel
+	format format.Format
+	core   *core.Core
 }
 
 // NewElog 创建一个日志组件
-func NewElog(logLevel LogLevel, core *core.Core) *Elog {
+func NewElog(logLevel LogLevel, format format.Format, core *core.Core) *Elog {
 	elog := &Elog{
-		Level: logLevel,
-		core:  core,
+		Level:  logLevel,
+		core:   core,
+		format: format,
 	}
 	elog.core.Start()
 
@@ -41,40 +45,46 @@ func (el *Elog) Debug(format string, a ...any) {
 	if el.Level > Debug {
 		return
 	}
-	el.core.Write([]byte(fmt.Sprintf(format, a...)))
+	log := el.format.DebugFormat(time.Now(), "", fmt.Sprintf(format, a...))
+	el.core.Write(log)
 }
 
 func (el *Elog) Info(format string, a ...any) {
 	if el.Level > Info {
 		return
 	}
-	el.core.Write([]byte(fmt.Sprintf(format, a...)))
+	log := el.format.InfoFormat(time.Now(), "", fmt.Sprintf(format, a...))
+	el.core.Write(log)
 }
 
 func (el *Elog) Warn(format string, a ...any) {
 	if el.Level > Warn {
 		return
 	}
-	el.core.Write([]byte(fmt.Sprintf(format, a...)))
+	log := el.format.WarnFormat(time.Now(), "", fmt.Sprintf(format, a...))
+	el.core.Write(log)
 }
 
 func (el *Elog) Error(format string, a ...any) {
 	if el.Level > Error {
 		return
 	}
-	el.core.Write([]byte(fmt.Sprintf(format, a...)))
+	log := el.format.ErrorFormat(time.Now(), "", fmt.Sprintf(format, a...))
+	el.core.Write(log)
 }
 
 func (el *Elog) Fatal(format string, a ...any) {
 	if el.Level > Fatal {
 		return
 	}
-	el.core.Write([]byte(fmt.Sprintf(format, a...)))
+	log := el.format.FatalFormat(time.Now(), "", fmt.Sprintf(format, a...))
+	el.core.Write(log)
 }
 
 func (el *Elog) Panic(format string, a ...any) {
 	if el.Level > Panic {
 		return
 	}
-	el.core.Write([]byte(fmt.Sprintf(format, a...)))
+	log := el.format.PanicFormat(time.Now(), "", fmt.Sprintf(format, a...))
+	el.core.Write(log)
 }
